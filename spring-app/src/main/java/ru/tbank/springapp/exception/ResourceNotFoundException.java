68b7fc7dfcba820fbb4ab8f766f44880941af6cf
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-09-30 19:53:10 +0300

    lesson 5: add ResourceNotFoundException

---
+++ spring-app/src/main/java/ru/tbank/springapp/exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package ru.tbank.springapp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException() {
+        this("Resource not found");
+    }
+
+}
