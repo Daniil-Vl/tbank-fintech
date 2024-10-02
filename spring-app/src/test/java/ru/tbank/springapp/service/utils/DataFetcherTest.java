3d5cf8440cd7ac5332b599afd229844171b457fb
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-10-02 20:28:03 +0300

    lesson 7: add test for DataFetcher

---
+++ spring-app/src/test/java/ru/tbank/springapp/service/utils/DataFetcherTest.java
@@ -0,0 +1,34 @@
+package ru.tbank.springapp.service.utils;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.tbank.springapp.client.KudagoClient;
+import ru.tbank.springapp.dao.Repository;
+import ru.tbank.springapp.model.Category;
+import ru.tbank.springapp.model.City;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class DataFetcherTest {
+
+    @MockBean
+    private KudagoClient client;
+
+    @MockBean
+    private Repository<String, Category> categoryRepository;
+
+    @MockBean
+    private Repository<String, City> cityRepository;
+
+    @Test
+    void testDataFetchingOnApplicationReadyEvent() {
+        verify(client, times(1)).getCategories();
+        verify(client, times(1)).getCities();
+    }
+}
