ffa97ec50162306e115d52b0c32590ee3624b3f7
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-10-20 13:22:05 +0300

    lesson 9: implemented event controller

---
+++ spring-app/src/main/java/ru/tbank/springapp/controller/EventController.java
@@ -0,0 +1,35 @@
+package ru.tbank.springapp.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Flux;
+import ru.tbank.springapp.dto.events.EventDTO;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public interface EventController {
+
+    @Operation(summary = "Получение событий на основе пользовательских пожеланий", operationId = "getEvents")
+    @GetMapping("/events-async")
+    CompletableFuture<List<EventDTO>> getEvents(
+            @RequestParam BigDecimal budget,
+            @RequestParam String currency, 
+            @RequestParam(required = false) @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate fromDate,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate toDate
+    );
+
+    @Operation(summary = "Получение событий на основе пользовательских пожеланий", operationId = "getEvents")
+    @GetMapping("/events-reactive")
+    Flux<EventDTO> getEventsReactive(
+            @RequestParam BigDecimal budget,
+            @RequestParam String currency,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate fromDate,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate toDate
+    ) throws InterruptedException;
+
+}
