a3b5409f571b5c135f4a9ee44832da15d9e5e29c
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-10-28 22:39:34 +0300

    lesson 10: add validation to PlaceDTO and EventDTO corresponding to db constraints

--- spring-app/src/main/java/ru/tbank/springapp/dto/EventDTO.java
+++ spring-app/src/main/java/ru/tbank/springapp/dto/EventDTO.java
@@ -2,6 +2,7 @@ package ru.tbank.springapp.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 import ru.tbank.springapp.model.entities.EventEntity;
 
 import java.time.LocalDate;
@@ -9,8 +10,8 @@ import java.time.LocalDate;
 public record EventDTO(
         @NotNull Long id,
         @NotNull LocalDate date,
-        @NotBlank String name,
-        @NotBlank String slug,
+        @NotBlank @Length(max = 300) String name,
+        @NotBlank @Length(max = 255) String slug,
         @NotBlank String placeName
 ) {
     public static EventDTO fromEvent(EventEntity event) {
--- spring-app/src/main/java/ru/tbank/springapp/dto/PlaceDTO.java
+++ spring-app/src/main/java/ru/tbank/springapp/dto/PlaceDTO.java
@@ -1,12 +1,13 @@
 package ru.tbank.springapp.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 import ru.tbank.springapp.model.City;
 import ru.tbank.springapp.model.entities.PlaceEntity;
 
 public record PlaceDTO(
-        @NotBlank String slug,
-        @NotBlank String name
+        @NotBlank @Length(max = 255) String slug,
+        @NotBlank @Length(max = 255) String name
 ) {
     public static PlaceDTO fromPlaceEntity(PlaceEntity placeEntity) {
         return new PlaceDTO(placeEntity.getSlug(), placeEntity.getName());
