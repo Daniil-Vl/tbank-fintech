94cdb0188bd657b0a084e95bf983831a2ed52635
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-10-30 16:13:45 +0300

    lesson 11: restore original data fetcher

--- spring-app/src/main/java/ru/tbank/springapp/service/utils/DataFetcher.java
+++ spring-app/src/main/java/ru/tbank/springapp/service/utils/DataFetcher.java
@@ -3,34 +3,49 @@ package ru.tbank.springapp.service.utils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import ru.tbank.springapp.aspect.Timed;
-import ru.tbank.springapp.patterns.observer.ClientPublisher;
-import ru.tbank.springapp.patterns.observer.Subscriber;
+import ru.tbank.springapp.client.KudagoClient;
+import ru.tbank.springapp.dao.Repository;
+import ru.tbank.springapp.dto.CategoryDTO;
+import ru.tbank.springapp.dto.CityDTO;
+import ru.tbank.springapp.model.Category;
+import ru.tbank.springapp.model.City;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 class DataFetcher {
 
-    private final ClientPublisher clientPublisher;
-    private final Subscriber categoriesRepositorySubscriber;
-    private final Subscriber citiesRepositorySubscriber;
-
-    @EventListener(ApplicationStartedEvent.class)
-    void makeSubscriptions() {
-        clientPublisher.subscribe(categoriesRepositorySubscriber);
-        clientPublisher.subscribe(citiesRepositorySubscriber);
-    }
+    private final KudagoClient kudagoClientImpl;
+    private final Repository<String, Category> categoryRepository;
+    private final Repository<String, City> cityRepository;
 
     @Timed
     @EventListener(ApplicationReadyEvent.class)
     void fetchData() {
         log.info("Fetching data from Kudago API...");
-        clientPublisher.getCategories();
-        clientPublisher.getCities();
+
+        List<CategoryDTO> categories = kudagoClientImpl.getCategories();
+        log.info("Fetched categories: {}", categories);
+
+        List<CityDTO> cities = kudagoClientImpl.getCities();
+        log.info("Fetched cities: {}", cities);
+
+        log.info("Saving fetched data...");
+
+        categories.forEach(categoryDTO -> {
+            categoryRepository.save(categoryDTO.slug(), categoryDTO.toCategory());
+        });
+
+        cities.forEach(cityDTO -> {
+            cityRepository.save(cityDTO.slug(), cityDTO.toCity());
+        });
+
+        log.info("Fetched data successfully saved");
     }
 
 }
