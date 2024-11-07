0d07bc4904dfdb7403ac598a062d7c4c8bad0a04
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-11-07 16:36:02 +0300

    lesson 13: add migrations for users and jwt tables

---
+++ spring-app/src/main/resources/migrations/changesets/create-jwt-table-changeset.sql
@@ -0,0 +1,11 @@
+--changeset Daniil-Vl:4
+
+CREATE TABLE jwt
+(
+    id      BIGSERIAL PRIMARY KEY NOT NULL,
+    token   TEXT                  NOT NULL,
+    user_id BIGINT REFERENCES users (id),
+    revoked BOOLEAN               NOT NULL
+)
+
+-- rollback DROP TABLE jwt
