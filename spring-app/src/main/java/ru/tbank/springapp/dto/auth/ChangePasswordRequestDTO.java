c291f8c6d6bba0b973fb189a311ec7d7248b9480
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-11-16 15:12:26 +0300

    lesson 13: add password validation to all dto

--- spring-app/src/main/java/ru/tbank/springapp/dto/auth/ChangePasswordRequestDTO.java
+++ spring-app/src/main/java/ru/tbank/springapp/dto/auth/ChangePasswordRequestDTO.java
@@ -1,11 +1,15 @@
 package ru.tbank.springapp.dto.auth;
 
 import jakarta.validation.constraints.NotBlank;
+import ru.tbank.springapp.validation.annotation.ValidPassword;
 
 public record ChangePasswordRequestDTO(
         @NotBlank
+        @ValidPassword
         String oldPassword,
+
         @NotBlank
+        @ValidPassword
         String newPassword
 ) {
 }
--- spring-app/src/main/java/ru/tbank/springapp/dto/auth/UserRegisterRequestDTO.java
+++ spring-app/src/main/java/ru/tbank/springapp/dto/auth/UserRegisterRequestDTO.java
@@ -2,14 +2,18 @@ package ru.tbank.springapp.dto.auth;
 
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
+import ru.tbank.springapp.validation.annotation.ValidPassword;
 
 public record UserRegisterRequestDTO(
         @NotBlank
         @Length(max = 255)
         String username,
+
         @NotBlank
+        @ValidPassword
         @Length(max = 255)
         String password,
+
         boolean rememberMe
 ) {
 }
